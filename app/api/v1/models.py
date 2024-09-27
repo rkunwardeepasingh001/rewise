89381e021cfb4975fe976353356e1f5dbf4ea056
rkunwardeepsingh001 <rkunwardeepsingh@gmail.com>
2024-09-27 12:43:12 +0530

    new2 file add

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -7,3 +7,10 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
+class FirstItem(BaseModel):
+    id: int
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
